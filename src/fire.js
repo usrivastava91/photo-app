a0a718ae8071621f30899867d431881befac10fe
Utkarsh <utkarsh@MacBook-Pro.local>
2020-01-11 16:51:48 +0530

    Initial Setup.
    1. Create React App with typescript, redux, react-router.
    2. Firebase Setup.

---
+++ src/fire.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+var config = {
+  apiKey: "AIzaSyAh2byLZOSwjzfJJRhrFvKVkx5rj9QFoGs",
+  authDomain: "photo-app-typito.firebaseapp.com",
+  databaseURL: "https://photo-app-typito.firebaseio.com",
+  storageBucket: "your-domain-name.appspot.com",
+  projectId: "photo-app-typito",
+  storageBucket: "photo-app-typito.appspot.com",
+  messagingSenderId: "537969035405",
+  appId: "1:537969035405:web:f2fa83115ad6c407203ac4",
+  measurementId: "G-87FS2EZJYF"
+};
+var fire = firebase.initializeApp(config);
+export default fire;
